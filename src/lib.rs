b20a8cf3ff0af90ebba9915a4a1e3882e9de4a99
Sojan James <sojan.james@gmail.com>
2021-03-09 22:08:07 +0530

    MIT license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+/*
+The MIT License (MIT)
+
+Copyright © 2021 Sojan James
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the “Software”), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial 
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
@@ -29,7 +50,7 @@ impl<T> ASNBox<T>
 where
     T: Sized + ASN1GenType,
 {
-    /// Create a boxed DDS type from a buffer that is allocated by
+    /// Create a boxed  type from a buffer that is allocated by asn1c
     pub unsafe fn new_from_asn1codec_allocated_struct(p: *mut T) -> Self {
         if !p.is_null() {
             Self(AllocatedData::<T>::Asn1CodecAllocated(p))
@@ -38,7 +59,7 @@ where
         }
     }
 
-    /// Build a ASNBox from a heap allocated DDSStructure
+    /// Build a ASNBox from a heap allocated struct 
     pub fn new_from_box(b: Box<T>) -> Self {
         Self(AllocatedData::RustAllocated(Box::into_raw(b)))
     }
